a359cfaca9b5b02abb8f504cef2528fbbfc6c248
Jake Shilling <shilling.jake@gmail.com>
2021-11-30 21:00:23 -0500

    Add quicklisp

--- config/nyxt/init.lisp
+++ config/nyxt/init.lisp
@@ -1,1 +1,11 @@
-(load-after-system :slynk "~/.config/nyxt/my-slynk.lisp")
+(in-package #:nyxt-user)
+
+;;; Load quicklisp. Not sure it works.
+#-quicklisp
+(let ((quicklisp-init
+       (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+(ql:quickload :slynk)
+
+(load-after-system :slynk "~/.config/nyxt/slynk.lisp")
