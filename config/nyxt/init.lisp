8e6b9a4583e2ef9d613485bc2db0fca72ad93972
Jake Shilling <shilling.jake@gmail.com>
2021-12-07 08:37:42 -0500

    Add dracula nyxt theme

--- config/nyxt/init.lisp
+++ config/nyxt/init.lisp
@@ -10,9 +10,12 @@
 (ql:quickload :slynk)
 (load-after-system :slynk "~/.config/nyxt/slynk.lisp")
 
-(define-configuration browser
-  ((session-restore-prompt :never-restore)))
+(dolist (file (list (nyxt-init-file "statusline.lisp")
+                    (nyxt-init-file "stylesheet.lisp")))
+  (load file))
 
+(define-configuration browser
+    ((session-restore-prompt :never-restore)))
 (define-configuration (buffer internal-buffer editor-buffer )
   ((default-modes `(vi-normal-mode ,@%slot-default%))))
 (define-configuration prompt-buffer
