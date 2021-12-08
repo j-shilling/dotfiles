f555eac6d8c5b1aee42063e6443a8b465fdbc6a0
Jake Shilling <shilling.jake@gmail.com>
2021-12-07 21:01:38 -0500

    Add dragula nyxt theme

--- config/nyxt/init.lisp
+++ config/nyxt/init.lisp
@@ -3,7 +3,7 @@
 ;;; Load quicklisp. Not sure it works.
 #-quicklisp
 (let ((quicklisp-init
-       (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
+        (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
