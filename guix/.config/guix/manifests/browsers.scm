cb6844b5ba6a8887217ac70b895d756777793428
Jake Shilling <shilling.jake@gmail.com>
2022-05-03 21:42:35 -0400

    Add media and browsers manifests

---
+++ guix/.config/guix/manifests/browsers.scm
@@ -0,0 +1,22 @@
+(define-module (manifests browsers)
+  #:use-module (gnu packages web-browsers)
+  #:use-module (gnu packages chromium)
+  #:use-module (gnu packages gnuzilla)
+  #:use-module (guix profiles)
+  #:export (browsers-packages
+            browsers-manifest))
+
+(when (resolve-module '(nongnu packages mozilla) #:ensure #f)
+  (use-modules (nongnu packages mozilla)))
+
+(define browsers-packages
+  (list nyxt
+        ungoogled-chromium
+        (if (defined? 'firefox)
+            firefox
+            icecat)))
+
+(define browsers-manifest
+  (packages->manifest browsers-packages))
+
+browsers-manifest
--- guix/.config/guix/profiles/default.scm
+++ guix/.config/guix/profiles/default.scm
@@ -1,14 +1,16 @@
 (define-module (profiles default)
   #:use-module (guix profiles)
   #:use-module (manifests doom-emacs)
-  #:use-module (manifests media)
   #:use-module (manifests core)
+  #:use-module (manifests media)
+  #:use-module (manifests browsers)
   #:export (default))
 
 (define default
   (concatenate-manifests
    (list doom-emacs-manifest
+         core-manifest
          media-manifest
-         core-manifest)))
+         browsers-manifest)))
 
 default
