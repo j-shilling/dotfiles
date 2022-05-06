2529e0059210f2f24472962c1b2e7db39952e98c
Jake Shilling <shilling.jake@gmail.com>
2022-05-06 11:07:03 -0400

    Add RDE channel

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -29,4 +29,13 @@
     "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
     (openpgp-fingerprint
      "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+ ;; Guix HOME etc
+ (channel
+  (name 'rde)
+  (url "https://git.sr.ht/~abcdw/rde")
+  (introduction
+   (make-channel-introduction
+    "257cebd587b66e4d865b3537a9a88cccd7107c95"
+    (openpgp-fingerprint
+     "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
  %default-channels)
