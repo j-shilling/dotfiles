ebb60cc9ba1aab80704671d962aa37e13b882ec1
Jake Shilling <shilling.jake@gmail.com>
2022-05-13 09:06:29 -0400

    FIX unbound var

--- guix/.config/guix/home/services/gpg.scm
+++ guix/.config/guix/home/services/gpg.scm
@@ -11,9 +11,8 @@
               (home-gpg-agent-configuration
                (ssh-agent? #t)
                (ssh-keys
-                (ssh-keys-list
-                 '(("57CCEEB098F2AA6791BA6D8F4CEF32B3F147C678")
-                   ("BA4555615BAE014981589CE5FA8A56E69156CDBE")
-                   ("6B626401361800A59771759B5FBF44151969E206"))))))))))
+                '(("57CCEEB098F2AA6791BA6D8F4CEF32B3F147C678")
+                  ("BA4555615BAE014981589CE5FA8A56E69156CDBE")
+                  ("6B626401361800A59771759B5FBF44151969E206")))))))))
 
 services
