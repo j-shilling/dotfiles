4e0b069ae85b6596c97b9f00f9033c7009ef20b3
Jake Shilling <shilling.jake@gmail.com>
2022-05-19 21:02:04 -0400

    Remove unused channels

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -10,32 +10,32 @@
     (openpgp-fingerprint
      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
  ;; Emacs 28
- (channel
-  (name 'flat)
-  (url
-   "https://github.com/flatwhatson/guix-channel.git")
-  (introduction
-   (make-channel-introduction
-    "33f86a4b48205c0dc19d7c036c85393f0766f806"
-    (openpgp-fingerprint
-     "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+ ;; (channel
+ ;;  (name 'flat)
+ ;;  (url
+ ;;   "https://github.com/flatwhatson/guix-channel.git")
+ ;;  (introduction
+ ;;   (make-channel-introduction
+ ;;    "33f86a4b48205c0dc19d7c036c85393f0766f806"
+ ;;    (openpgp-fingerprint
+ ;;     "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
  ;; Minecraft
- (channel
-  (name 'guix-gaming-games)
-  (url
-   "https://gitlab.com/guix-gaming-channels/games.git")
-  (introduction
-   (make-channel-introduction
-    "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
-    (openpgp-fingerprint
-     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+ ;; (channel
+ ;;  (name 'guix-gaming-games)
+ ;;  (url
+ ;;   "https://gitlab.com/guix-gaming-channels/games.git")
+ ;;  (introduction
+ ;;   (make-channel-introduction
+ ;;    "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+ ;;    (openpgp-fingerprint
+ ;;     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
  ;; Guix Home Etc.
- (channel
-  (name 'rde)
-  (url "https://git.sr.ht/~abcdw/rde")
-  (introduction
-   (make-channel-introduction
-    "257cebd587b66e4d865b3537a9a88cccd7107c95"
-    (openpgp-fingerprint
-     "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+ ;; (channel
+ ;;  (name 'rde)
+ ;;  (url "https://git.sr.ht/~abcdw/rde")
+ ;;  (introduction
+ ;;   (make-channel-introduction
+ ;;    "257cebd587b66e4d865b3537a9a88cccd7107c95"
+ ;;    (openpgp-fingerprint
+ ;;     "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
  %default-channels)
