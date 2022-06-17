ad9506101ca5c1f5361214056d86d8faec31ad2c
Jake Shilling <shilling.jake@gmail.com>
2022-06-16 21:06:14 -0400

    Re-enable games channel

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -20,15 +20,15 @@
  ;;    (openpgp-fingerprint
  ;;     "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
  ;; Minecraft
- ;; (channel
- ;;  (name 'guix-gaming-games)
- ;;  (url
- ;;   "https://gitlab.com/guix-gaming-channels/games.git")
- ;;  (introduction
- ;;   (make-channel-introduction
- ;;    "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
- ;;    (openpgp-fingerprint
- ;;     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+ (channel
+  (name 'guix-gaming-games)
+  (url
+   "https://gitlab.com/guix-gaming-channels/games.git")
+  (introduction
+   (make-channel-introduction
+    "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+    (openpgp-fingerprint
+     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
  ;; Guix Home Etc.
  ;; (channel
  ;;  (name 'rde)
