a8ee2f4fc1b3821ad183413caa80e475c08c2b8c
Jake Shilling <shilling.jake@gmail.com>
2022-08-04 14:31:51 -0400

    debug init

--- emacs-sandbox.sh
+++ emacs-sandbox.sh
@@ -8,4 +8,4 @@ guix environment \
     --share=/tmp/.X11-unix/ \
     --share="$HOME/dotfiles/emacs/.emacs.d/=$HOME/.emacs.d" \
     --share="$HOME/.config/guix/=$HOME/.config/guix" \
-    --manifest="$HOME/.config/guix/manifests/emacs.scm" -- emacs
+    --manifest="$HOME/.config/guix/manifests/emacs.scm" -- emacs --debug-init
