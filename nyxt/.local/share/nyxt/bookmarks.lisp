4ee3c1dc64f76d97d1b4519559ffbc919cd08b7c
Jake Shilling <shilling.jake@gmail.com>
2022-08-10 15:36:31 -0400

    Add vallen bookmarks

--- nyxt/.local/share/nyxt/bookmarks.lisp
+++ nyxt/.local/share/nyxt/bookmarks.lisp
@@ -1,4 +1,12 @@
 (
- (:url "https://meet.google.com/iwi-drup-zej" :title "Ivory Tower" :tags ("ISE"))
- (:url "https://securityevaluators.atlassian.net/" :title "Jira" :tags ("ISE"))
+ ;; Vallen
+ (:url "https://sonepar.employee.eu.people-doc.com/home"
+  :title "Sonapar People Portal"
+  :tags ("Vallen"))
+ (:url "https://dev.azure.com/vallenit"
+  :title "Azure DevOps"
+  :tags ("Vallen"))
+ (:url "https://adb-2116111370301711.11.azuredatabricks.net/login.html"
+  :title "Databricks Sandbox"
+  :tags ("Vallen"))
 )
