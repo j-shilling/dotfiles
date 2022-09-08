d6168500a9d155d085da5e165aa740fe36587202
Jake Shilling <shilling.jake@gmail.com>
2022-09-08 08:56:25 -0400

    Replace firefox specification with package

--- guix/.config/guix/jrs/manifests/browsers.scm
+++ guix/.config/guix/jrs/manifests/browsers.scm
@@ -1,13 +1,15 @@
 (define-module (jrs manifests browsers)
   #:use-module (gnu packages)
   #:use-module (guix profiles)
+  #:use-module (nongnu packages mozilla)
   #:export (packages
             manifest))
 
 (define packages
-  (map (compose list specification->package+output)
-       '("nyxt"
-         "firefox")))
+  (append
+   (map (compose list specification->package+output)
+        '("nyxt"))
+   `((,firefox "out"))))
 
 (define manifest
   (packages->manifest packages))
