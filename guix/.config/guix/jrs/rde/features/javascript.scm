1ab5f2898c58fae1bc9c3974fb15aaa9d291e5b8
Jake Shilling <shilling.jake@gmail.com>
2022-10-12 09:29:53 -0400

    Add javascript feature

---
+++ guix/.config/guix/jrs/rde/features/javascript.scm
@@ -0,0 +1,53 @@
+(define-module (jrs rde features javascript)
+  #:use-module (rde features)
+  #:use-module (rde features predicates)
+  #:use-module (rde features emacs)
+
+  #:use-module (gnu packages node)
+  #:use-module (gnu packages emacs-xyz)
+
+  #:use-module (gnu services)
+  #:use-module (gnu home services)
+
+  #:export (feature-javascript))
+
+(define* (feature-javascript
+          #:key
+          (node-pkg node-lts))
+  "Setup and configure environment for JavaScript/TypeScript."
+  (ensure-pred any-package? node)
+
+  (define (get-home-services config)
+    (let ((emacs-f-name 'javascript))
+      (list
+       (when (get-value 'emacs config)
+         (rde-elisp-configuration-service
+          emacs-f-name
+          config
+          '((dolist (hook '(js-mode-hook
+                            typescript-mode-hook))
+                    (add-hook hook
+                              (lambda ()
+                                (setq tab-width 2)))
+                    (add-hook hook 'eglot)))
+          #:summary "\
+JavaScript/TypeScript configuration and pacakges."
+          #:commentary "\
+Configuration for JavaScript/TypeScript development with node and LSP."
+          #:keywords '(javascript typescript)
+          #:elisp-packages
+          (list emacs-typescript-mode
+                emacs-nodejs-repl
+                (get-value 'emacs-eglot config emacs-eglot))))
+
+       (simple-service
+        'packages-for-javascript
+        home-profile-service-type
+        (list
+         node-pkg)))))
+
+  (feature
+   (name 'javascript)
+   (values `((javascript . #t)
+             (typescript . #t)))
+   (home-services-getter get-home-services)))
