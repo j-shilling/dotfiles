c356433e6e0459a00b8faf59da034e6d7893e01f
Jake Shilling <shilling.jake@gmail.com>
2022-10-20 21:11:58 -0400

    FIX broken package builds

--- jrs/packages/freedesktop.scm
+++ jrs/packages/freedesktop.scm
@@ -1,32 +1,66 @@
 (define-module (jrs packages freedesktop)
+  #:use-module (rde packages)
+  #:use-module (gnu packages gl)
   #:use-module ((gnu packages freedesktop) :prefix gnu:)
   #:use-module ((jrs packages linux) :prefix jrs:)
   #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages gnome)
-  #:use-module (gnu packages linux))
-
-(define pipewire jrs:pipewire)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages cran))
 
 (define-public xdg-desktop-portal
   (package
    (inherit gnu:xdg-desktop-portal)
    (name "xdg-desktop-portal")
    (version "1.14.16")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append
+                  "https://github.com/flatpak/xdg-desktop-portal/releases/download/"
+                  version "/xdg-desktop-portal-" version ".tar.xz"))
+            (sha256
+             (base32
+              "0wqc9x3k7lf3mig53i4rjazi0xi8bcykwaaw7r7prvnscnd1k405"))))
    (inputs
     (modify-inputs (package-inputs gnu:xdg-desktop-portal)
-                   (replace "pipewire" pipewire)))))
+                   (replace "pipewire" jrs:pipewire)))))
+
+(define-public xdg-desktop-portal-gtk
+  (package
+   (inherit gnu:xdg-desktop-portal-gtk)
+   (propagated-inputs
+    (modify-inputs (package-propagated-inputs gnu:xdg-desktop-portal-gtk)
+                   (replace "xdg-desktop-portal" xdg-desktop-portal)))))
 
 (define-public xdg-desktop-portal-wlr
   (package
-   (inherit gnu:xdg-desktop-portal)
-   (name "xdg-desktop-portal-wlr")
-   (version "1.14.16")
-   (inputs
-    (modify-inputs (package-inputs gnu:xdg-desktop-portal-wlr)
-                   (replace "pipewire" pipewire)
-                   (prepend glib json-glib gnu:libportal geoclue
-                            fuse-3 gdk-pixbuf dbus)))))
+    (inherit gnu:xdg-desktop-portal-wlr)
+    (name "xdg-desktop-portal-wlr")
+    (version "0.6.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/emersion/xdg-desktop-portal-wlr")
+                    (commit (string-append "v" version))))
+              (file-name
+               (git-file-name (package-name gnu:xdg-desktop-portal-wlr) version))
+              (sha256
+               (base32
+                "1b02708fiz9195n71xlw8iy9dghmb5iynxppfx7wjvh8ydz68fsk"))
+              (patches
+               (search-patches "xdg-desktop-portal-wlr-harcoded-length.patch"))))
+    (inputs
+     (modify-inputs (package-inputs gnu:xdg-desktop-portal-wlr)
+                    (replace "pipewire" jrs:pipewire)
+                    (replace "wayland-protocols" gnu:wayland-protocols-next)
+                    (prepend mesa libdrm-latest)))
+    (propagated-inputs
+     (list xdg-desktop-portal
+           xdg-desktop-portal-gtk))))
 
 xdg-desktop-portal
--- jrs/packages/linux.scm
+++ jrs/packages/linux.scm
@@ -1,16 +1,36 @@
 (define-module (jrs packages linux)
   #:use-module ((gnu packages linux) :prefix gnu:)
-  #:use-module (guix packages))
+  #:use-module (guix packages)
+  #:use-module (guix git-download))
 
 (define-public pipewire
   (package
    (inherit gnu:pipewire-0.3)
-   (version "0.3.59")))
+   (version "0.3.59")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/PipeWire/pipewire")
+                  (commit version)))
+            (file-name (git-file-name (package-name gnu:pipewire-0.3) version))
+            (sha256
+             (base32
+              "17jnj1las9wnizwh6g9ix2pxnna029vj66qwhrnij19315vfs073"))))))
 
 (define-public wireplumber
   (package
    (inherit gnu:wireplumber)
    (version "0.4.12")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url
+            "https://gitlab.freedesktop.org/pipewire/wireplumber.git")
+           (commit version)))
+     (file-name (git-file-name (package-name gnu:wireplumber) version))
+     (sha256
+      (base32 "0k6ylqq4r1xzc7bbmwy1qgwpqra8shdvilwm1pvzzvilgyqkm8ys"))))
    (inputs
     (modify-inputs (package-inputs gnu:wireplumber)
                    (replace "pipewire" pipewire)))))
