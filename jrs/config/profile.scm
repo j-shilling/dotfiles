5fea023f3c0697ef44b26e183842e9430ba520ef
Jake Shilling <shilling.jake@gmail.com>
2022-10-21 23:13:16 -0400

    Add polymc

--- jrs/config/profile.scm
+++ jrs/config/profile.scm
@@ -1,16 +1,20 @@
 (define-module (jrs config profile)
+  #:use-module (jrs packages polymc)
   #:use-module (gnu packages))
 
 (define-public %home-packages
-  (map specification->package+output
-          '("bluez"
-            "flatpak"
-            "make"
-            "nyxt"
-            "ungoogled-chromium"
-            "zathura"
-            "zathura-pdf-poppler"
-            "qemu")))
+  (append
+   (map specification->package+output
+        '("bluez"
+          "flatpak"
+          "make"
+          "nyxt"
+          "ungoogled-chromium"
+          "zathura"
+          "zathura-pdf-poppler"
+          "qemu"))
+   (list
+    polymc)))
 
 (define-public %system-packages
   (map specification->package+output
