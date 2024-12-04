fbfe1b72254689fab7aee044d6ba77b82b3ca0ee
Jake Shilling <shilling.jake@gmail.com>
2024-12-04 07:16:38 -0500

    Add more emacs settings

--- src/config/emacs.scm
+++ src/config/emacs.scm
@@ -4,6 +4,7 @@
   #:use-module (rde features emacs-xyz)
   #:use-module (rde home services emacs)
   #:use-module (gnu packages emacs)
+  #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu services)
   #:export (emacs-features))
 
@@ -32,7 +33,85 @@
             (use-package emacs
                          :custom
                          (user-full-name ,(get-value 'full-name config))
-                         (user-mail-address ,(get-value 'email config)))))))))
+                         (user-mail-address ,(get-value 'email config))
+
+                         (use-short-answers t)
+                         (ring-bell-function (function ignore))
+
+                         (auto-save-default t)
+                         (auto-save-timeout 20)
+                         (auto-save-interval 200)
+
+                         (make-backup-files t)
+                         (vc-make-backup-files nil)
+                         (backup-by-copying t)
+                         (version-control t)
+                         (keep-old-versions 6)
+                         (keep-new-versions 9)
+
+                         (delete-by-moving-to-trash nil)
+
+                         (kill-ring-max 120)
+                         (kill-do-not-save-duplicate t)
+
+                         (vc-follow-symlinks t)
+
+                         (save-interprogram-paste-before-kill t)
+                         (mouse-yank-at-point t)
+
+                         (require-final-newline t)
+
+                         (text-mode-ispell-word-completion nil)
+                         (sentence-end-double-space nil)
+
+                         :init
+                         (dolist (fn '(set-default-coding-systems
+                                       prefer-coding-system
+                                       set-terminal-coding-system
+                                       set-keyboard-coding-system
+                                       set-buffer-file-coding-system
+                                       set-selection-coding-system))
+                                 (apply fn (list 'utf-8-unix)))
+                         (set-language-environment "English")
+
+                         (setq-default indent-tabs-mode nil)
+                         (setq-default tab-width 4))))
+         (elisp-packages (list emacs-use-pacakge
+                               emacs-diminish))))))
+
+  (feature
+   (name f-name)
+   (values `((,f-name . #t)))
+   (home-services-getter  get-home-services)))
+
+(define (feature-emacs-performance-config)
+  (define emacs-f-name 'performance-config)
+  (define f-name (symbol-append 'emacs- emacs-f-name))
+
+  (define (get-home-services config)
+    (list
+     (simple-service
+        'emacs-extensions
+        home-emacs-service-type
+        (home-emacs-extension
+         (init-el
+          `((eval-when-compile
+             (require 'use-package))
+            (use-package emacs
+                         :custom
+                         (large-file-warning-threshold nil))
+            (use-package vlf
+                         :init
+                         (require 'vlf-setup))
+            (use-package gcmh
+                         :diminish gcmh-mode
+                         :hook (after-init . gcmh-mode))
+            (use-package so-long
+                         :diminish global-so-long-mode
+                         :hook (after-init . global-so-long-mode))))
+         (elisp-packages (list emacs-vlf
+                               emacs-so-long
+                               emacs-gcmh))))))
 
   (feature
    (name f-name)
@@ -52,6 +131,7 @@
       #:default-terminal? #f
       #:default-application-launcher? #f))
    (feature-emacs-base-config)
+   (feature-emacs-vlf-config)
    (feature-emacs-appearance)
    (feature-emacs-modus-themes
     #:dark? #t)
