4c92d81cb1f201d33c95a2ddcca632f8dbdd2908
Jake Shilling <shilling.jake@gmail.com>
2025-03-08 16:29:21 -0500

    upgrade

--- channels-lock.scm
+++ channels-lock.scm
@@ -1,17 +1,6 @@
 (use-modules (guix channels))
 
 (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "310adf4ce70cbb864859274fcc7842bd519bbddc")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
         (name 'rde)
         (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
@@ -23,11 +12,22 @@
             (openpgp-fingerprint
               "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
       (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "42bc94adbd1114771ef14a8e19563829ae355e87")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
         (name 'non-guix)
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "d0b6099324cd47961064bd96ae830c4ca6d1bada")
+          "944619c1947ba59823f4ad6d8b13842b7e162d76")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
