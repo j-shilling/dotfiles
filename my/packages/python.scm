2f857f159cd044bf99639162b51bbeebfc129bff
Jake Shilling <shilling.jake@gmail.com>
2025-03-15 18:47:26 -0400

    work on python package

--- my/packages/python.scm
+++ my/packages/python.scm
@@ -1,7 +1,16 @@
 (define-module (my packages python)
   #:use-module (guix packages)
   #:use-module (guix gexp)
+  #:use-module (gnu packages)
   #:use-module (gnu packages python))
+
+(define patches
+  (string-append
+                 (if (current-filename)
+                     (dirname (current-filename))
+                     (string-append (getenv "HOME")
+                                    "/dotfiles/my/packages"))
+                 "/patches/"))
 
 (define-public python-3.11
   (package
@@ -14,4 +23,8 @@
                                   version "/Python-" version ".tar.xz"))
               (sha256
                (base32
-                "11svljymlyx5rmcxmjvak0g3s2inv43491kc2a8hclgw4djqj7lv"))))))
+                "11svljymlyx5rmcxmjvak0g3s2inv43491kc2a8hclgw4djqj7lv"))
+              (patches (cons
+                        (string-append patches "python-3.11-fix-tests.patch")
+                        (search-patches "python-3-deterministic-build-info.patch"
+                                        "python-3-hurd-configure.patch")))))))
